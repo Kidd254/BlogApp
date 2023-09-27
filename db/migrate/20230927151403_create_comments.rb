9125f71f8d6690604e406c9051adc9db5cb4c79c
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-27 21:01:14 +0300

    Add foreign keys and fix errors

--- db/migrate/20230927151403_create_comments.rb
+++ db/migrate/20230927151403_create_comments.rb
@@ -1,17 +1,11 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-        t.integer :user_id
-        t.integer :post_id
-        t.text :text
-        t.datetime :updated_at
-        t.datetime :created_at
-  
-        t.timestamps
-      end
-  
-      add_index :comments, :user_id
-      add_index :comments, :post_id
+      t.text :text
+      t.timestamps 
+      t.references :user, foreign_key: true 
+      t.references :post, foreign_key: true
     end
   end
+end
   
--- db/migrate/20230927151437_create_likes.rb
+++ db/migrate/20230927151437_create_likes.rb
@@ -1,16 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :user_id
-      t.integer :post_id
-      t.datetime :updated_at
-      t.datetime :created_at
-
-      t.timestamps
+      t.timestamps 
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true 
     end
-
-    add_index :likes, :user_id
-    add_index :likes, :post_id
   end
-end # END: abpxx6d04wxr
-  end
+end
