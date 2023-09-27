15b3aca7a6b457ae8b1c6f47ceffa1f8707f4a12
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-27 18:32:02 +0300

    Define table structure for comments table and establish relationships with other tables (likes, posts,  and user)

--- db/migrate/20230927151403_create_comments.rb
+++ db/migrate/20230927151403_create_comments.rb
@@ -1,8 +1,17 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-
-      t.timestamps
+        t.integer :user_id
+        t.integer :post_id
+        t.text :text
+        t.datetime :updated_at
+        t.datetime :created_at
+  
+        t.timestamps
+      end
+  
+      add_index :comments, :user_id
+      add_index :comments, :post_id
     end
   end
 end
