8be2ba1e13ec35d2f6fba006dfe5c15c97410064
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-27 18:34:04 +0300

    Define table structure for like table and establish its relationship with user

--- db/migrate/20230927151437_create_likes.rb
+++ db/migrate/20230927151437_create_likes.rb
@@ -1,8 +1,16 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
+      t.integer :user_id
+      t.integer :post_id
+      t.datetime :updated_at
+      t.datetime :created_at
 
       t.timestamps
+    end
+
+    add_index :likes, :user_id
+    add_index :likes, :post_id
     end
   end
 end
