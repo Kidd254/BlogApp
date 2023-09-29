a60e93af73aa190121039900ae5a5330793d3848
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-29 12:32:51 +0300

    Establish tables relationships using model and add relevant methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :user
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  after_create :update_user_posts_counter
+
+  def update_user_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
