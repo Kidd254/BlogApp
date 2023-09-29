d177fb0b54fdce17988ae7640b3d480549a7b273
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-29 12:53:45 +0300

    fix linters

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,4 +1,10 @@
-require "test_helper"
+require 'test_helper'
+
+class LikeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
 
 class LikeTest < ActiveSupport::TestCase
   # test "the truth" do
