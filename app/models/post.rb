06907eebf52c9add6cf0496fdba586a018bca16a
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-02 23:24:02 +0300

    Update posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ApplicationRecord
   after_create :update_user_posts_counter
 
   def update_user_posts_counter
-    user.update(posts_counter: user.posts.count)
-  end
+    author.update(posts_counter: author.posts.count)
+  end  
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
