bda16db02bba8c28dad41aee183d4608ce644282
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-03 01:42:04 +0300

    Add  requested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
   resources :users, param: :name, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
