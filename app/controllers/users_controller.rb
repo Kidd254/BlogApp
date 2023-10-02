8d970f02bac9e67087a97fa07f8e056de74b2f67
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-02 23:21:45 +0300

    Add methods to post controllers to display list of user and posts and details for each

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+  include ApplicationHelper
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(name: params[:name])
+    if @user.nil?
+      flash[:error] = "User not found"
+      redirect_to '/' 
+    end
   end
 end
