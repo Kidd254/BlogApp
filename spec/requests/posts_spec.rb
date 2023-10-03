8ec94bf77f9cc0775434b74f00b1cc745f3c040b
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-03 20:02:56 +0300

    Add tests for controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+  describe 'GET /users/:name/posts' do
+    let(:user) { create(:user) }
+    let(:post) { create(:post, author: user) }
+
+    it 'returns a successful response' do
+      user_name = CGI.escape(user.name)
+      get "/users/#{user_name}/posts"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      user_name = CGI.escape(user.name)
+      get "/users/#{user_name}/posts"
+      expect(response).to render_template('posts/index')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+  describe 'GET /users/:name/posts/:id' do
+    let(:user) { create(:user) }
+    let(:post) { create(:post, author: user) }
+
+    it 'returns a successful response' do
+      user_name = CGI.escape(user.name)
+      get "/users/#{user_name}/posts/#{post.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      user_name = CGI.escape(user.name)
+      get "/users/#{user_name}/posts/#{post.id}"
+      expect(response).to render_template('posts/show')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+  describe 'GET /users' do
+    it 'returns a successful response' do
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      get '/users'
+      expect(response).to render_template('users/index')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('All Users') # Update to match your actual text
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+  describe 'GET /users/:name' do
+    let(:user) { create(:user, name: 'ValidUsername') } # Replace 'ValidUsername' with an actual valid username
+
+    it 'returns a successful response' do
+      get "/users/#{user.name}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      get "/users/#{user.name}"
+      expect(response).to render_template('users/show')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get "/users/#{user.name}"
+      expect(response.body).to include('User Profile') # Update to match your actual text
     end
   end
 end
+
