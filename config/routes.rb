9519786ed6846ac019c59b5652dc2c864414be3d
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-04 20:33:03 +0300

    Add rountes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users, param: :name, only: [:index, :show] do
     resources :posts, only: [:index, :show]
+    resources :comments, only: [:index, :show, :create, :update, :destroy]
+resources :likes, only: [:create, :destroy]
   end
 end
