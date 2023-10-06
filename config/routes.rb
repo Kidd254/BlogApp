444e2ffe65db989db4cd634fa3a1a98770c7ee6e
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-06 09:48:11 +0300

    Update routes to resolve local host error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'users#index'
+  
   resources :users, param: :name, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-    resources :comments, only: [:index, :show, :create, :update, :destroy]
-resources :likes, only: [:create, :destroy]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:create, :update, :destroy]
+      resources :likes, only: [:create, :destroy]
+    end
+
+    # Define the post route within the user's resources block
+    get 'posts/:id', to: 'posts#show', on: :member, as: 'post'
   end
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post', on: :member
 end
