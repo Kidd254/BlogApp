68d441e17e067815b1ac46ef79e1a87c1fb1e363
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-06 12:18:14 +0300

    Add routes for likes, and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   root 'users#index'
-  
+
   resources :users, param: :name, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
       resources :comments, only: [:create, :update, :destroy]
+
+      # Add routes for liking and unliking a post
+      post 'like', to: 'likes#create'
+      delete 'unlike', to: 'likes#destroy'
+
+      # Define the post route within the posts resources block
+      get 'posts/:id', to: 'posts#show', on: :member, as: 'post'
       resources :likes, only: [:create, :destroy]
     end
-
-    # Define the post route within the user's resources block
-    get 'posts/:id', to: 'posts#show', on: :member, as: 'post'
   end
 end
+
+
+
