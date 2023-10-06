cdb1d37f98de8753b6fdc295bd3ee34b03dd1eca
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-06 12:16:48 +0300

    Add methods for creating new posts, and saving posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,32 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def index
-    @users = User.all # Fetch all users
+    @users = User.all
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
