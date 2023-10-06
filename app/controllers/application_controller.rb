77bf1759f744b473349b1a8dc8e443e2292c5faf
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-06 12:14:48 +0300

    Correct linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :set_current_user
+  before_action :set_current_user
 
   def set_current_user
     @current_user = User.first
