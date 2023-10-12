82fa41bb105a8959f60179f2039c878ec4aed1d9
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-12 22:40:50 +0300

    Add confirm_At column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_142237) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_12_193421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_142237) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "confirmed_at"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
