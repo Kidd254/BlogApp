58fbdd0c0bb235ebaa9131c27a60f03dc0b5451c
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-14 11:47:18 +0300

    Add permisions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,17 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # Guest user (not logged in)
-
+    
     if user.admin?
-      can :manage, :all # Admins can manage all resources
+      can :manage, Post
+      can :manage, Comment
     else
-      can :manage, Post, user_id: user.id
-      can :manage, Comment, user_id: user.id
+      
+      can :read, :all
+      can :create, Post, author_id: user.id
+      can :create, Comment
+      can :destroy, Comment, author_id: user.id
+      can :destroy, Post, author_id: user.id
     end
   end
 end
