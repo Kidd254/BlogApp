26d1a4af5cae7efce7cc6e3b05f6e7cdc9b812b6
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-14 11:50:31 +0300

    Keep users credentials in form of cookies

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module BlogApp
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.session_store :cookie_store, key: '_blog_app_session'
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
